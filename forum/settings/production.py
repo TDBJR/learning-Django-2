8bfa8f0af290c39740ef8cf92af00e202ebf7133
Thomas <ThomasDBurner@Gmail.com>
2017-01-27 17:23:13 -0800

    some init msg

--- forum/settings/production.py
+++ forum/settings/production.py
@@ -3,14 +3,16 @@ from django.conf import settings
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 ALLOWED_HOSTS = ['*']
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT =  'staticfiles'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-)
+    )
