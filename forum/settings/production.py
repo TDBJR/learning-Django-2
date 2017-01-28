f176828a0687bbb07c32b84d6849d7eb86011473
Thomas <ThomasDBurner@Gmail.com>
2017-01-27 17:58:17 -0800

    some init msg

--- forum/settings/production.py
+++ forum/settings/production.py
@@ -1,18 +1,32 @@
 import os
 from django.conf import settings
-
+import dj_database_url
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
+#===============================================================================
+# db_from_env = dj_database_url.config()
+# DATABASES=['default'].update(db_from_env)
+#===============================================================================
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
+    }
+}
+
+DATABASES['default'] = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = ['*']
+
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-    )
+)
